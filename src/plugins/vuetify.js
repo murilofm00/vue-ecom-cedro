6ccc2b5e7b558c855bd7f8a1dcbc9da7ef77fb51
Murilo Fernandes <murilofm@unipam.edu.br>
2020-01-23 15:49:50 -0300

    Paleta de cores

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: '#34403F',
+            secondary: '#488C87',
+            accent: '#728C8A',
+          },
+        },
+      },
 });
