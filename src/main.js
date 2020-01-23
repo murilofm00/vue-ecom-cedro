00ffec05688cccd598b4c012bf14d253b9a7a5da
Murilo Fernandes <murilofm@unipam.edu.br>
2020-01-22 22:20:39 -0300

    Instalação Vuetify e Firebase

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCzEFByxTnIAV5RRS1WJhpf0Pm6iPrUtiU",
+  authDomain: "vue-ecom-a769d.firebaseapp.com",
+  databaseURL: "https://vue-ecom-a769d.firebaseio.com",
+  projectId: "vue-ecom-a769d",
+  storageBucket: "vue-ecom-a769d.appspot.com",
+  messagingSenderId: "151146164868",
+  appId: "1:151146164868:web:3090adce76e167932488d1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
