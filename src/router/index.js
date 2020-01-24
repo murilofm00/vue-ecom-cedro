5b5532c9c50a5184e3390d7c173a32bae997108e
Murilo Fernandes <murilofm@unipam.edu.br>
2020-01-23 22:51:24 -0300

    pagina de login(layoult)

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Home from '@/views/Home.vue'
+import Login from '@/components/Login.vue'
+
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: About
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
